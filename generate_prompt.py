1be330abe367914b1cad133b7c4e26a3ff0c7eda
YongbaekCho <105251370+YongbaekCho@users.noreply.github.com>
2023-05-04 14:11:24 -0700

    remove unnecessary

--- generate_prompt.py
+++ generate_prompt.py
@@ -8,39 +8,32 @@ model_id = "gpt-3.5-turbo"
 IG = 'instagram'
 
 
-def get_information(info):
-    """
-    get the informative data from Do and Don't
-    """
-    prompt = """
-           Please provide the top 5 most informative points from the list of Do's and Don'ts provided respectively, 
-           which must include the use of FTC disclosure and relevant hashtags. 
-            """.format(info)
-    completion = openai.ChatCompletion.create(model=model_id, messages=[{"role": "user", "content": prompt}])
-    return completion["choices"][0]["message"]["content"]
 
 
-def generate_prompts(brief, network_type, constraint):
+
+def generate_prompts(content_brief, network_type, constraint):
     # prompt = '''
     #         write a one high-quality prompt for generating an {} image post based on the given {} and {}.
     #         The prompt should be no more than 200 words long and should effectively guide the generative model to generate a post
     #          that accurately represents the given the above text and constraints.
-    #         '''.format(network_type, brief, constraint)
+    #         '''.format(network_type, content_brief, constraint)
     prompt = '''
     I want to get a prompt that starts with "please write a prompt ~" to chatgpt to generate the post text.
     Please write a prompt for generating an {} post based on the given this text {} and this info {}, 
     the required hashtags from info must be included and no more than 200 words! 
-    '''.format(network_type, brief, constraint)
+    '''.format(network_type, content_brief, constraint)
 
     completion = openai.ChatCompletion.create(model=model_id, messages=[{"role": "user", "content": prompt}])
 
     return completion["choices"][0]["message"]["content"]
+
 
 def generate_post_text(input_prompt, network_type, post_type):
     prompt_for_test_generation = '''Please write realistic {} {} text with required hashtags and emojis based on this information {}. 
     no more than 100 words!'''.format(network_type, post_type, input_prompt)
 
-    completion = openai.ChatCompletion.create(model=model_id, messages=[{"role": "user", "content": prompt_for_test_generation}])
+    completion = openai.ChatCompletion.create(model=model_id,
+                                              messages=[{"role": "user", "content": prompt_for_test_generation}])
 
     return completion["choices"][0]["message"]["content"]
 
@@ -77,12 +70,21 @@ if __name__ == "__main__":
     DON’T go live prior to review and approval from the Linqia team.
     DON’T reference violence, politics, explicit content, profanity, or specific religious holidays within all content.
     '''
-    #
-    # most_top_five_do_and_dont = get_information(info)
-    # print(most_top_five_do_and_dont)
 
     prompt_result_from_gpt = generate_prompts(brief, IG, info)
     print('#################################################Prompt for chat gpt#######################################')
     print(prompt_result_from_gpt)
     print('#################################################post text#################################################')
     print(generate_post_text('post', IG, prompt_result_from_gpt))
+
+#
+# def get_information(info):
+#     """
+#     get the informative data from Do and Don't
+#     """
+#     prompt = """
+#            Please provide the top 5 most informative points from the list of Do's and Don'ts provided respectively,
+#            which must include the use of FTC disclosure and relevant hashtags.
+#             """.format(info)
+#     completion = openai.ChatCompletion.create(model=model_id, messages=[{"role": "user", "content": prompt}])
+#     return completion["choices"][0]["message"]["content"]
